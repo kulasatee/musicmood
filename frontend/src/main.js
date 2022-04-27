88142efc53c6875c78addc2334728f45548d3824
kulasatee <kulasateedul@hotmail.com>
2022-04-28 03:05:18 +0700

    add: frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import './css/style.css'
+import './js/script.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
